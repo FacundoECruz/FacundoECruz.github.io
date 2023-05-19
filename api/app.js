ff2ddbb077c60b7635f27326212668b0520b5d89
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-05-19 12:42:43 -0300

    [wip] connecting with react

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path")
 const Player = require("./models/Player")
 
 mongoose.connect("mongodb://localhost:27017/altisima", {
@@ -14,19 +15,30 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
-app.get("/", async (req, res) => {
-  res.send("hello");
-  const player = new Player({
-    name: "Facu",
-    score: 10,
-  })
-  await player.save()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
-app.all('*', (req, res) => {
-  res.send("not found")
-})
+app.get("/", async (req, res) => {
+  res.send();
+});
 
 app.listen("3000", () => {
   console.log('ON 3000')
 });
+
+
+
+// app.get("/login", (req, res) => {})
+// app.post("/login", (req, res) => {})
+// app.get("/player", (req, res) => {})
+// app.post("player", (req, res) => {})
+// app.get("/player/:id", (req, res) => {})
+// app.get("/game", (req, res) => {})
+// app.post("/game", (req, res) => {})
+// app.get("/game/:id", (req, res) => {})
--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function App() {
-  return (
-    <h1>Altisima</h1>
-  )
-}
