e184f0f48a9adc260f48d9f7bab1a374e66dc48e
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-05-22 17:07:22 -0300

    seeded db with players

--- api/models/Player.js
+++ api/models/Player.js
@@ -1,11 +1,34 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
 
 const PlayerSchema = new Schema({
-  username: String,
-  image: String,
+  username: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default:
+      "https://play-lh.googleusercontent.com/rX_nOuUDijsV_NnWZP9JgYTsFpxn5y7qCqDxFIpZ-BqiJu8un7UbdSgVTZSrJuzAlQ",
+  },
   password: String,
-})
+  gamesWon: {
+    type: Number,
+    default: 0,
+  },
+  gamesPlayed: {
+    type: Number,
+    default: 0,
+  },
+  createdGames: {
+    type: Number,
+    default: 0,
+  },
+  totalScore: {
+    type: Number,
+    default: 0,
+  },
+});
 
 const Player = mongoose.model("Player", PlayerSchema);
 
---
+++ notas.md
@@ -0,0 +1,12 @@
+# Notas
+
+### Game
+
+- Como medir el tiempo de partida? En que formato guardarlo?
+- Que vamos a usar para la fecha? Moment.js?
+- En /game/new vamos a tener que tener una instancia donde pidamos los jugadores, validemos sus nombres y pidamos la cantidad de rondas.
+- Que estructura va a tener la data de la partida? Teniendo en cuenta que tienen que guardarse los resultados de cada una de las rondas.
+
+### Player
+- Seedear la base de datos.
+
