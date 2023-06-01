65cd9a03853aea36dd009e358f58947b664652a3
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-06-01 13:53:39 -0300

    rendering player's grid

--- src/pages/NewGame/NewGame.jsx
+++ src/pages/NewGame/NewGame.jsx
@@ -1,7 +1,20 @@
+import players from "../../assets/players.js";
+import PlayerGrid from "./PlayerGrid.jsx";
+import "../../stylesheets/NewGame.css";
+
 function NewGame() {
-  return ( 
-    <h1>New Game page</h1>
-   );
+
+  return (
+    <>
+      <h1>New Game page</h1>
+      <main>
+        {players.map((p) => {
+          return <PlayerGrid key={p.name} player={p} onSubmit={handleRound}/>;
+        })}
+      </main>
+      <button onClick={handleRound}>Next Round</button>
+    </>
+  );
 }
 
 export default NewGame;
