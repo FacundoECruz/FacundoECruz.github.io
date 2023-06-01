451b089d3111944ac4fd9210ab09aa2a722fc07b
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-06-01 17:36:46 -0300

    created player schema

---
+++ api/models/Player.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const PlayerSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwUmQOGfv3HMATaiRo8hDCdcu23Otwqg2pEg&usqp=CAU",
+  },
+  gamesWon: {
+    type: Number,
+    default: 0,
+  },
+  gamesPlayed: {
+    type: Number,
+    default: 0,
+  },
+  totalScore: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Player = mongoose.model("Player", PlayerSchema);
+
+export default Player;
