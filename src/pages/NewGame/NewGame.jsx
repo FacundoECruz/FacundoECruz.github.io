4632f4bbb045ab535879128b68c11c7b7ef62b88
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-06-01 17:22:10 -0300

    deleted objects

--- src/pages/NewGame/NewGame.jsx
+++ src/pages/NewGame/NewGame.jsx
@@ -1,4 +1,3 @@
-import players from "../../assets/players.js";
 import PlayerGrid from "./PlayerGrid.jsx";
 import "../../stylesheets/NewGame.css";
 
@@ -7,12 +6,7 @@ function NewGame() {
   return (
     <>
       <h1>New Game page</h1>
-      <main>
-        {players.map((p) => {
-          return <PlayerGrid key={p.name} player={p} onSubmit={handleRound}/>;
-        })}
-      </main>
-      <button onClick={handleRound}>Next Round</button>
+      <PlayerGrid />
     </>
   );
 }
--- src/pages/NewGame/PlayerGrid.jsx
+++ src/pages/NewGame/PlayerGrid.jsx
@@ -1,43 +1,32 @@
 import "../../stylesheets/PlayerGrid.css";
-import { useEffect, useState } from "react";
 
-function PlayerGrid(player) {
-  const { name, bet, lose, score, history } = player.player;
-
-  let [playerBet, setPlayerBet] = useState(bet)
-  let [playerLose, setPlayerLose] = useState(lose)
-  let [playerScore, setPlayerScore] = useState(score)
-  let [playerBetHistory, setPlayerBetHistory] = useState(history)
-
-  useEffect(() => {
-    console.log(player.player);
-  });
+function PlayerGrid() {
 
   return (
     <section className="player-grid-container">
       <div className="player-header">
-        <h2>{name}</h2>
-        <h3>{playerScore}</h3>
+        {/* <h2>{name}</h2> */}
+        {/* <h3>{playerScore}</h3> */}
       </div>
       <div className="player-history">
-        {playerBetHistory.map((res, i) => {
+        {/* {playerBetHistory.map((res, i) => {
           return <p key={i} className={res > 0 ? "win" : "lose"}>{res}</p>
-        })}
+        })} */}
       </div>
       <div className="controls-container">
         <p>Apuesta</p>
-        <h3>{playerBet}</h3>
+        {/* <h3>{playerBet}</h3> */}
         <div className="buttons-container">
-          <button onClick={() => setPlayerBet(playerBet += 1)}>+</button>
-          <button onClick={() => setPlayerBet(playerBet -= 1)}>-</button>
+          {/* <button onClick={() => setPlayerBet(playerBet += 1)}>+</button> */}
+          {/* <button onClick={() => setPlayerBet(playerBet -= 1)}>-</button> */}
         </div>
       </div>
       <div className="controls-container">
         <p>Pierde</p>
-        <h3>{playerLose}</h3>
+        {/* <h3>{playerLose}</h3> */}
         <div className="buttons-container">
-          <button onClick={() => setPlayerLose(playerLose += 1)}>+</button>
-          <button onClick={() => setPlayerLose(playerLose -= 1)}>-</button>
+          {/* <button onClick={() => setPlayerLose(playerLose += 1)}>+</button> */}
+          {/* <button onClick={() => setPlayerLose(playerLose -= 1)}>-</button> */}
         </div>
       </div>
     </section>
