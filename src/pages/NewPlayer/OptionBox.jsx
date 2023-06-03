1ba25f84a678af5a6f016bd5fb977742c846b355
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-06-03 14:03:35 -0300

    setted grid layout practice

--- src/pages/NewPlayer/OptionBox.jsx
+++ src/pages/NewPlayer/OptionBox.jsx
@@ -1,19 +1,29 @@
 /* eslint-disable react/prop-types */
-import { Button, Container, Typography, Box } from "@mui/material";
+import { Button, Container, Typography, Box, Grid } from "@mui/material";
 
 function OptionBox(props) {
-
-  const {message, buttonVariant, buttonText, icon, color} = props
+  const { message, buttonVariant, buttonText, icon, color } = props;
 
   return (
     <Container maxWidth="sm">
       <Box sx={{ border: 1, boxShadow: 3, p: 1 }}>
-        <Typography color="primary" variant="h5" textAlign="center" m={4}>
-          {message}
-        </Typography>
-        <Button color={color} variant={buttonVariant} sx={{ m: 1 }} startIcon={icon}>
-          {buttonText}
-        </Button>
+        <Grid container direction="column" justifyContent="center" align="center">
+          <Grid item>
+            <Typography color="primary" variant="h5" textAlign="center" m={4}>
+              {message}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button
+              color={color}
+              variant={buttonVariant}
+              sx={{ m: 1 }}
+              startIcon={icon}
+            >
+              {buttonText}
+            </Button>
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
--- src/pages/NewPlayer/index.jsx
+++ src/pages/NewPlayer/index.jsx
@@ -1,35 +1,44 @@
 import OptionBox from "./OptionBox.jsx";
-import { Typography } from "@mui/material";
+import { Typography, Grid } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import ConnectWithoutContactIcon from "@mui/icons-material/ConnectWithoutContact";
-import LightModeIcon from '@mui/icons-material/LightMode';
+import LightModeIcon from "@mui/icons-material/LightMode";
 
 function NewPlayer() {
   return (
     <>
-      <Typography textAlign="center" variant="h2">
+      <Typography textAlign="center" variant="h2" mb={5}>
         Crear Jugador
       </Typography>
-      <OptionBox
-        message="Crear nueva cuenta"
-        buttonVariant="contained"
-        buttonText="New"
-        icon={<AddIcon />}
-        color="primary"
-      />
-      <OptionBox
-        message="Asociar cuenta con jugador existente"
-        buttonVariant="outlained"
-        buttonText="Asociate"
-        icon={<ConnectWithoutContactIcon />}
-      />
-      <OptionBox
-        message="Ver al brillan"
-        buttonVariant="contained"
-        buttonText="Brillan"
-        icon={<LightModeIcon />}
-        color="warning"
-      />
+
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={6}>
+          <OptionBox
+            message="Crear nueva cuenta"
+            buttonVariant="contained"
+            buttonText="New"
+            icon={<AddIcon />}
+            color="primary"
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <OptionBox
+            message="Asociar cuenta con jugador existente"
+            buttonVariant="outlained"
+            buttonText="Asociate"
+            icon={<ConnectWithoutContactIcon />}
+          />
+        </Grid>
+        <Grid item xs={12} md={12}>
+          <OptionBox
+            message="Ver al brillan"
+            buttonVariant="contained"
+            buttonText="Brillan"
+            icon={<LightModeIcon />}
+            color="warning"
+          />
+        </Grid>
+      </Grid>
     </>
   );
 }
