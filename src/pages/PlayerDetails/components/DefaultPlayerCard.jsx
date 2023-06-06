08236295b3d42a154fa92e8b40c43d8a9392e899
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-06-06 12:26:22 -0300

    added conditional card rendering

--- src/pages/PlayerDetails/components/DefaultPlayerCard.jsx
+++ src/pages/PlayerDetails/components/DefaultPlayerCard.jsx
@@ -1,15 +1,25 @@
 import { Container, Grid, Box } from "@mui/material";
 import PlayedGameData from "./PlayedGameData";
 import PlayerCard from "../../../components/PlayerCard";
+import PlayerCardSkeleton from "../../../components/PlayerCard/PlayerCardSkeleton";
 import { player } from "./fakePlayerData";
 
 function DefaultPlayerCard() {
+
   return (
     <Container>
       <Grid container mt={4} spacing={2}>
         <Grid item xl={6}>
           <Box sx={{ display: "grid", gap: 2, maxWidth: 250, mx: "auto" }}>
-            <PlayerCard image={player.image} username={player.username} level={player.level}/>
+            {player ? (
+              <PlayerCard
+                image={player.image}
+                username={player.username}
+                level={player.level}
+              />
+            ) : (
+              <PlayerCardSkeleton />
+            )}
           </Box>
         </Grid>
         <Grid item xl={6} md={6}>
