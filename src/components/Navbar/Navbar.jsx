e8e9b3b1c23f8cc0d1bf89b280a30ed3ca6d2f6a
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-06-06 08:46:18 -0300

    fixed navbar overlap

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import {
   AppBar,
+  Box,
   Button,
   Drawer,
   IconButton,
@@ -9,8 +10,8 @@ import {
 import NavListDrawer from "./NavListDrawer";
 import { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
-import InboxIcon from "@mui/icons-material/Inbox"
-import DraftsIcon from "@mui/icons-material/Drafts"
+import InboxIcon from "@mui/icons-material/Inbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
 
 const navLinks = [
   {
@@ -40,33 +41,41 @@ function Navbar() {
 
   return (
     <>
-      <AppBar position="fixed">
+      <AppBar position="static">
         <Toolbar>
           <IconButton
             color="inherit"
             size="large"
             onClick={() => setOpen(true)}
+            sx={{ display: { xs: "flex", sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
             Altisima
           </Typography>
-          {navLinks.map((item) => (
-            <Button
-              color="inherit"
-              key={item.title}
-              component="a"
-              href={item.path}
-            >
-              {item.title}
-            </Button>
-          ))}
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navLinks.map((item) => (
+              <Button
+                color="inherit"
+                key={item.title}
+                component="a"
+                href={item.path}
+              >
+                {item.title}
+              </Button>
+            ))}
+          </Box>
         </Toolbar>
       </AppBar>
 
-      <Drawer open={open} anchor="left" onClose={() => setOpen(false)}>
-        <NavListDrawer navLinks={navLinks}/>
+      <Drawer
+        open={open}
+        anchor="left"
+        onClose={() => setOpen(false)}
+        sx={{ display: { xs: "flex", sm: "none" } }}
+      >
+        <NavListDrawer navLinks={navLinks} />
       </Drawer>
     </>
   );
--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,19 +1,22 @@
-import Navbar from "../../components/Navbar/Navbar.jsx"
-import MainHome from "./MainHome.jsx"
-import Text from "./TextBody.jsx"
-import Faq from "./FAQ.jsx"
-import Footer from "../../components/Footer.jsx"
-import "../../stylesheets/Home.css"
+import Navbar from "../../components/Navbar/Navbar.jsx";
+import MainHome from "./MainHome.jsx";
+import Text from "./TextBody.jsx";
+import Faq from "./FAQ.jsx";
+import Footer from "../../components/Footer.jsx";
+import "../../stylesheets/Home.css";
+import { Container } from "@mui/material";
 
 function Home() {
   return (
-    <div className="home-container">
+    <>
       <Navbar />
-      <MainHome />
-      <Text />
-      <Faq />
-      <Footer />
-    </div>
+      <Container sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+        <MainHome />
+        <Text />
+        <Faq />
+        <Footer />
+      </Container>
+    </>
   );
 }
 
