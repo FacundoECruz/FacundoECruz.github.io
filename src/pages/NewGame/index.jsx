6661ec1b50e0dfc4edd9774168ee012d76430aa8
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-06-07 16:06:34 -0300

    showing added players in new game

--- src/pages/NewGame/index.jsx
+++ src/pages/NewGame/index.jsx
@@ -1,16 +1,30 @@
 import { useState } from "react";
 import api from "../../utils/api-client";
 import "../../stylesheets/NewGame.css";
-import { Box, Button, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  TextField,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 
 function NewGame() {
   const [name, setName] = useState("");
   const [players, setPlayers] = useState([]);
 
   async function handleChange(e) {
-    setName(e.target.value)
+    setName(e.target.value);
     // const allPlayers = await api.getPlayers()
     // console.log(allPlayers)
+  }
+
+  function addPlayer(e) {
+    e.preventDefault();
+    players.push(name);
+    setName("");
+    console.log(players);
   }
 
   return (
@@ -18,6 +32,8 @@ function NewGame() {
       <h1>New Game page</h1>
       <Box
         sx={{ display: "flex", flexDirection: "column" }}
+        component="form"
+        onSubmit={addPlayer}
       >
         <TextField
           id="text"
@@ -27,10 +43,10 @@ function NewGame() {
           required
           value={name}
           onChange={handleChange}
-          sx={{mx: 1}}
+          sx={{ mx: 1 }}
         />
-        <Box sx={{my: 2, mx: 1}}>
-          <Button variant="contained" sx={{ bgcolor: "green" }}>
+        <Box sx={{ my: 2, mx: 1 }}>
+          <Button type="submit" variant="contained" sx={{ bgcolor: "green" }}>
             Agregar
           </Button>
           <Button onClick={() => setPlayers([])} variant="contained">
@@ -38,6 +54,21 @@ function NewGame() {
           </Button>
         </Box>
       </Box>
+
+      {players.length ? (
+        <Box>
+          {players.map((p) => {
+            return (
+              <List key={p} sx={{ bgcolor: "green" }}>
+                <ListItem>
+                  <ListItemText>{p}</ListItemText>
+                </ListItem>
+              </List>
+            );
+          })} 
+          <Button>Empezar</Button>
+        </Box>
+      ) : null}
     </>
   );
 }
