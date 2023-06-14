5c4ffe1ec837021329bea0ab992d4b68fc8de4c8
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-06-14 18:59:23 -0300

    deleted new player route

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Index";
 import Players from "./pages/Players/Players";
 import PlayerDetails from "./pages/PlayerDetails";
-import NewPlayer from "./pages/NewPlayer";
 import Games from "./pages/Games/Games";
 import GameDetails from "./pages/GameDetails/GameDetails";
 import NewGame from "./pages/NewGame";
@@ -20,7 +19,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/players" element={<Players />} />
         <Route path="/players/:id" element={<PlayerDetails />} />
-        <Route path="/players/new" element={<NewPlayer />} />
         <Route path="/games" element={<Games />} />
         <Route path="/games/:id" element={<GameDetails />} />
         <Route path="/games/new" element={<NewGame />} />
