b59574a65ec09537e65c4eac8fb656cbd957ce6c
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-06-14 19:05:34 -0300

    added bg image to all games page

--- src/pages/Games/Games.jsx
+++ src/pages/Games/Games.jsx
@@ -1,7 +1,21 @@
+import { Typography } from "@mui/material";
+
 function Games() {
-  return ( 
-    <h1>All games page</h1>
-   );
+  return (
+    <div
+      style={{
+        position: "relative",
+        background:
+          "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://res.cloudinary.com/dfknsvqer/image/upload/v1686776553/zuza-galczynska-c5_eQi4rrjA-unsplash_gaz4ty.jpg')",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Typography variant="h5" sx={{color: "white"}}>Partidas Jugadas</Typography>
+    </div>
+  );
 }
 
 export default Games;
