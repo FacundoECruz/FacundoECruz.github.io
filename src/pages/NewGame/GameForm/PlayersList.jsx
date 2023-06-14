a8cbbe53e2a30239b3672b0baa8eb82156d080a3
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-06-14 18:59:51 -0300

    changed player list style in new game

--- src/pages/NewGame/GameForm/PlayersList.jsx
+++ src/pages/NewGame/GameForm/PlayersList.jsx
@@ -1,28 +1,46 @@
 /* eslint-disable react/prop-types */
-import { Box, List, ListItem, ListItemText, Button, ListItemAvatar,Avatar } from "@mui/material";
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Button,
+  ListItemAvatar,
+  Avatar,
+  Typography,
+} from "@mui/material";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
 
 function PlayersList({ players, setGameState }) {
   return (
     <Box sx={{ width: "50%" }}>
       {players.map((p) => {
         return (
-          <List key={p} sx={{ bgcolor: "green" }}>
+          <List key={p} sx={{ bgcolor: "transparent" }}>
             <ListItem>
               <ListItemAvatar>
                 <Avatar>
-                  <AccountBoxIcon sx={{bgcolor: "orange"}}/>
+                  <AccountBoxIcon sx={{ bgcolor: "orange" }} />
                 </Avatar>
               </ListItemAvatar>
-              <ListItemText primary={p} secondary="****" />
+              <ListItemText
+                primary={p}
+                secondary={
+                  <Typography variant="body2" sx={{ color: "yellow" }}>
+                    ***
+                  </Typography>
+                }
+                sx={{ color: "white" }}
+              />
             </ListItem>
           </List>
         );
       })}
-      <Button onClick={() => setGameState("inProgress")}>Empezar</Button>
+      {players.length > 2 ? (
+        <Button sx={{color: "white"}} onClick={() => setGameState("inProgress")}>Empezar</Button>
+      ) : null}
     </Box>
   );
 }
 
 export default PlayersList;
-
