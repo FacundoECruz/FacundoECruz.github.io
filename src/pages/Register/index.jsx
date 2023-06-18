8884cbdcf42c3bb9b5cddb2abbc76b11dde0b413
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-06-18 10:13:57 -0300

    added bg image to register

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,6 +1,5 @@
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
@@ -10,7 +9,6 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { InputLabel, OutlinedInput } from "@mui/material";
 
 function Copyright(props) {
@@ -31,8 +29,6 @@ function Copyright(props) {
   );
 }
 
-const defaultTheme = createTheme();
-
 export default function SignInSide() {
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -44,96 +40,106 @@ export default function SignInSide() {
     });
   };
 
+  const styles = {
+    paperContainer: {
+      background:
+        "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5)), url('https://res.cloudinary.com/dfknsvqer/image/upload/v1687093391/patrick-tomasso-QMDap1TAu0g-unsplash_qvsjkn.jpg')",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "right",
+    },
+  };
+
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
+    <Grid
+      container
+      component="main"
+      sx={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        md={5}
+        component={Paper}
+        elevation={6}
+        square
+        style={styles.paperContainer}
+      >
+        <Box
           sx={{
-            backgroundImage:
-              "url(https://res.cloudinary.com/dfknsvqer/image/upload/v1686742030/altisima/poker_lvo2km.jpg)",
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
+            my: 8,
+            mx: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        >
+          <Avatar sx={{ m: 1, bgcolor: "green" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5" sx={{color: "white"}}>
+            Registrarse
+          </Typography>
           <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 1, color: "white", }}
           >
-            <Avatar sx={{ m: 1, bgcolor: "green" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Registrarse
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              sx={{bgcolor: "white"}}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Contraseña"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              sx={{bgcolor: "white"}}
+            />
+            <InputLabel htmlFor="image-upload" sx={{color: "white"}}>Seleccionar imagen</InputLabel>
+            <OutlinedInput
+              id="image-upload"
+              type="file"
+              name="image"
+              inputProps={{
+                accept: "image/*",
+              }}
+              sx={{ ml: 2, color: "white" }}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Recordar usuario"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
             >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Contraseña"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <InputLabel htmlFor="image-upload">Seleccionar imagen</InputLabel>
-              <OutlinedInput
-                id="image-upload"
-                type="file"
-                name="image"
-                inputProps={{
-                  accept: "image/*",
-                }}
-                sx={{ ml: 2 }}
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Recordar usuario"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Registrarse
-              </Button>
-              <Copyright sx={{ mt: 5 }} />
-            </Box>
+              Registrarse
+            </Button>
+            <Copyright sx={{ mt: 5 }} />
           </Box>
-        </Grid>
+        </Box>
       </Grid>
-    </ThemeProvider>
+    </Grid>
   );
 }
