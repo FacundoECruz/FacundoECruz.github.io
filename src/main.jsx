7691e2cfc9dbb973903bb197e4dac19ef42ddc74
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-06-26 19:48:35 -0300

    disabled strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -23,12 +23,10 @@ const theme = createTheme({
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
     <ThemeProvider theme={theme}>
       <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
         <CssBaseline />
         <App />
       </SnackbarProvider>
     </ThemeProvider>
-  </React.StrictMode>
 );
