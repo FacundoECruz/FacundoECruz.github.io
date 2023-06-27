49b4075289f31f910296de108d1ed393eca80283
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-06-27 09:48:16 -0300

    added clean buttons functionality

--- src/pages/NewGame/Scores/index.jsx
+++ src/pages/NewGame/Scores/index.jsx
@@ -2,44 +2,48 @@
 import { Button, Typography } from "@mui/material";
 import { useEffect } from "react";
 import { useState } from "react";
-import { useReducer } from 'react';
+import { useReducer } from "react";
 import PlayerDash from "./PlayerDash";
 import api from "../../../utils/api-client";
-
-const types = {
-  addBid: "bid - add",
-  resetBid: "bid - reset",
-  addLost: "lost - add",
-  resetLost: "lost - reset",
-  clean: "round - clean",
-};
+import { types } from "../../../utils/reducer";
 
 function reducer(state, action) {
+  console.log(state);
   switch (action.type) {
     case types.addBid: {
-      return {
-        ...state,
-        bid: (state[action.index].bid += 1),
+      let newState = [...state];
+      newState[action.index] = {
+        ...newState[action.index],
+        bid: newState[action.index].bid + 1,
       };
+      return newState;
     }
     case types.resetBid: {
-      let newState = { ...state };
-      newState[action.index].bid = 0;
+      let newState = [...state];
+      newState[action.index] = {
+        ...newState[action.index],
+        bid: 0,
+      };
       return newState;
     }
     case types.addLost: {
-      return {
-        ...state,
-        bidsLost: (state[action.index].bidsLost += 1),
+      let newState = [...state];
+      newState[action.index] = {
+        ...newState[action.index],
+        bidsLost: newState[action.index].bidsLost + 1,
       };
+      return newState;
     }
     case types.resetLost: {
-      let newState = { ...state };
-      newState[action.index].bidsLost = 0;
+      let newState = [...state];
+      newState[action.index] = {
+        ...newState[action.index],
+        bidsLost: 0,
+      };
       return newState;
     }
     case types.clean: {
-      let newState = { ...state };
+      let newState = [...state];
       newState.map((p) => {
         p.bid = 0;
         p.bidsLost = 0;
@@ -52,15 +56,15 @@ function reducer(state, action) {
 }
 
 function Scores({ players, setGameState }) {
-  const [cardsInCurrent, setCardsInCurrent] = useState(0)
-  const [round, setRound] = useState(0)
-  const [playersRound, dispatch] = useReducer(reducer, players);
+  const [cardsInCurrent, setCardsInCurrent] = useState(0);
+  const [round, setRound] = useState(0);
+  const [playersRound, dispatch] = useReducer(reducer, [...players]);
 
   useEffect(() => {
-    const cards = window.localStorage.getItem("cardsInCurrent")
-    const round = window.localStorage.getItem("round")
-    setCardsInCurrent(cards)
-    setRound(round)
+    const cards = window.localStorage.getItem("cardsInCurrent");
+    const round = window.localStorage.getItem("round");
+    setCardsInCurrent(cards);
+    setRound(round);
   }, []);
 
   useEffect(() => {
@@ -68,27 +72,34 @@ function Scores({ players, setGameState }) {
   }, [playersRound]);
 
   function nextRound() {
-    const gameId = window.localStorage.getItem("gameId")
-    
-    api
-      .nextRound(gameId, playersRound)
+    const gameId = window.localStorage.getItem("gameId");
+
+    api.nextRound(gameId, playersRound);
   }
 
   return (
     <>
       <Typography variant="h4">Ronda {round + 1}</Typography>
       <Typography variant="h4">Cartas {cardsInCurrent}</Typography>
-      {players.map((p, i) => {
+      {playersRound.map((p, i) => {
         return (
-          <PlayerDash player={p} key={i} index={i} dispatch={dispatch} types={types}/>
-        )
+          <PlayerDash
+            player={p}
+            key={i}
+            index={i}
+            dispatch={dispatch}
+            types={types}
+          />
+        );
       })}
+
+      <Button onClick={() => dispatch({ type: types.clean })}>Limpiar</Button>
 
       <Button onClick={nextRound}>Siguiente Ronda</Button>
 
       <Button
         onClick={() => {
-          window.localStorage.removeItem("players")
+          window.localStorage.removeItem("players");
           setGameState("idle");
         }}
       >
