f0f6d8c6c259e8af4a5efdbf9c4457f117ec339f
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-07-10 12:42:38 -0300

    added hover to player cards

--- src/components/PlayerCard/index.jsx
+++ src/components/PlayerCard/index.jsx
@@ -3,16 +3,40 @@ import { Card, CardMedia, CardContent, Typography, Box } from "@mui/material";
 
 function PlayerCard({ image, username, stats, width, margin }) {
   return (
-    <Card sx={{ maxWidth: "200px", width: width, margin: margin, bgcolor: "blue" }}>
+    <Card
+      sx={{
+        maxWidth: "200px",
+        width: width,
+        margin: margin,
+        bgcolor: "blue",
+        transition: "transform 0.3s",
+        "&:hover": {
+          transform: "translate(-5px, -5px)",
+        },
+      }}
+    >
       <CardMedia image={image} title={username} sx={{ height: "150px;" }} />
       <CardContent>
-        <Typography variant="h6" sx={{ marginBottom: 1, whiteSpace: "nowrap", fontSize: username.length > 15 ? "12px" : "inherit", }}>
+        <Typography
+          variant="h6"
+          sx={{
+            marginBottom: 1,
+            whiteSpace: "nowrap",
+            fontSize: username.length > 15 ? "12px" : "inherit",
+          }}
+        >
           {username}
         </Typography>
-        <Box sx={{ display: "flex", justifyContent: "space-between", flexDirection: "column", }}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: "column",
+          }}
+        >
           {stats.map((stat, index) => (
             <Typography key={index} variant="body2" sx={{ fontWeight: "bold" }}>
-              {stat.label}: {stat.value}
+              {stat.label}: {isNaN(stat.value) ? "-" : stat.value}
             </Typography>
           ))}
         </Box>
