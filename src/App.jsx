9ad5205b054283ef19af2f4db616b99f2fc485c0
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-07-16 09:57:52 -0300

    [wip] solve folders issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "/pages/home/index.jsx";
-import Players from "/pages/players/Players.jsx";
-import PlayerDetails from "/pages/playerDetails/index.jsx";
-import Games from "/pages/games/Games.jsx";
-import GameDetails from "/pages/gameDetails/GameDetails.jsx";
-import NewGame from "/pages/newGame/index.jsx";
-import Login from "/pages/login/index.jsx";
-import Register from "/pages/register/index.jsx";
-import Navbar from "/components/navbar/Navbar.jsx";
-import "/App.css";
-import { AuthProvider } from "/utils/AuthContext";
-import RequireAuth from "/utils/requireAuth";
+import Players from "./pages/players/Players.jsx";
+import PlayerDetails from "./pages/playerDetails/index.jsx";
+import Games from "./pages/games/Games.jsx";
+import GameDetails from "./pages/gameDetails/GameDetails.jsx";
+import NewGame from "./pages/newGame/index.jsx";
+import Login from "./pages/login/index.jsx";
+import Register from "./pages/register/index.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
+import "./App.css";
+import { AuthProvider } from "./utils/AuthContext";
+import RequireAuth from "./utils/requireAuth";
 
 function App() {
 
