5ca26835379f3f1437a3f5b65ea25f50eff37006
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-07-16 11:20:42 -0300

    added base to config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build",
-    rollupOptions: {
-      external: [/^node:.*/],
-    },
-    baseDirectory: "/dist",
-  },
-});
+  base: "/FacundoECruz.github.io/"
+})
