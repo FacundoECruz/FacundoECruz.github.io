2e687dafb1ac7480f965f56aa76e9cd81f182e47
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-08-22 08:50:45 -0300

    [wip] set login base case testing

--- src/pages/login/__tests__/login.test.js
+++ src/pages/login/__tests__/login.test.js
@@ -3,30 +3,57 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Login from "..";
-import { AuthProvider } from "../../../utils/AuthContext";
 import { BrowserRouter as Router } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 
-const mockAuthContext = {
-  user: "mockUser",
-  login: jest.fn(),
-  loginError: null,
+const mockUseAuth = () => {
+  return {
+    user: "mockUser",
+    login: jest.fn(),
+    loginError: null,
+  };
 };
 
 describe("Login Component", () => {
   it("renders correctly", () => {
     render(
       <Router>
-        <AuthProvider value={mockAuthContext}>
-          <Login />
-        </AuthProvider>
+        <Login useAuth={mockUseAuth} />
       </Router>
     );
 
-    expect(screen.getByTestId("username")).toBeInTheDocument();
+    expect(
+      screen.getByRole("textbox", { name: /username/i })
+    ).toBeInTheDocument();
     expect(screen.getByTestId("password")).toBeInTheDocument();
     expect(
       screen.getByRole("button", { name: "Ingresar" })
     ).toBeInTheDocument();
+  });
+
+  it("submitting the form calls onSubmit with username and password", () => {
+    const useAuthMock = mockUseAuth();
+    render(
+      <Router>
+        <Login useAuth={mockUseAuth} />
+      </Router>
+    );
+
+    const username = "Facu";
+    const password = "noLaNecesito";
+
+    userEvent.type(
+      screen.getByRole("textbox", { name: /username/i }),
+      username
+    );
+    userEvent.type(screen.getByTestId("password"), password);
+    userEvent.click(screen.getByRole("button", { name: "Ingresar" }));
+
+    expect(useAuthMock.login).toHaveBeenCalledWith({
+      username,
+      password,
+    });
+    expect(useAuthMock.login).toHaveBeenCalledTimes(1);
   });
 });
