244428b1f1e39e31b3b308559a9a74f13fcb83ba
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-08-28 18:59:52 -0300

    modularized scores reducer

---
+++ src/pages/newGame/scores/scoresReducer.js
@@ -0,0 +1,52 @@
+import { types } from "../../../utils/reducerTypes";
+
+export function scoresReducer(state, action) {
+  switch (action.type) {
+    case types.addBid: {
+      let newState = [...state];
+      newState[action.index] = {
+        ...newState[action.index],
+        bid: newState[action.index].bid + 1,
+      };
+      return newState;
+    }
+    case types.resetBid: {
+      let newState = [...state];
+      newState[action.index] = {
+        ...newState[action.index],
+        bid: 0,
+      };
+      return newState;
+    }
+    case types.addLost: {
+      let newState = [...state];
+      newState[action.index] = {
+        ...newState[action.index],
+        bidsLost: newState[action.index].bidsLost + 1,
+      };
+      return newState;
+    }
+    case types.resetLost: {
+      let newState = [...state];
+      newState[action.index] = {
+        ...newState[action.index],
+        bidsLost: 0,
+      };
+      return newState;
+    }
+    case types.clean: {
+      let newState = [...state];
+      newState.map((p) => {
+        p.bid = 0;
+        p.bidsLost = 0;
+      });
+      return newState;
+    }
+    case types.nextRound: {
+      let newState = action.newState;
+      return newState;
+    }
+    default:
+      return state;
+  }
+}
---
+++ src/utils/reducerTypes.js
@@ -0,0 +1,8 @@
+export const types = {
+  addBid: "bid - add",
+  resetBid: "bid - reset",
+  addLost: "lost - add",
+  resetLost: "lost - reset",
+  clean: "round - clean",
+};
+
