cb6eea6108e6e904ff6b182360951c32944fb59f
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-10-03 15:32:27 -0300

    fixed backend link

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -1,29 +1,32 @@
 import axios from "axios";
 
 const server = axios.create({
+  // baseURL: "http://localhost:8080",
   // baseURL: "http://localhost:3000/api",
-  baseURL: "https://altisima-scoreboard.onrender.com/api",
+  // baseURL: "https://altisima-scoreboard.onrender.com/api",
+  baseURL: "https://altisima-spring-backend.onrender.com",
 });
 
+//OJO CON LOS V1 CUANDO CAMBIAMOS DE SERVIDOR!!!
+
 const api = {
-  getGame: () => server.get("/games"),
-  createGame: (players) => server.post("/games", players),
   getGames: () => server.get("/games"),
+  createGame: (players) => server.post("/v1/games", players),
   getPlayer: (username) => server.get(`/players/${username}`),
-  createPlayer: (playerData) => server.post("/players", playerData),
+  createPlayer: (playerData) => server.post("/v1/players", playerData),
   getPlayers: () => server.get("/players"),
   getUser: (username) => server.get(`/users/${username}`),
   login: (data) => server.post("/users/login", data),
-  createUser: (userData) => server.post("/users", userData),
+  createUser: (userData) => server.post("/v1/users", userData),
   getUsers: () => server.get("/users"),
   editUser: (username, userData) =>
-    server.patch(`/users/${username}`, userData),
-  deleteUser: (id) => server.delete(`/users/${id}`),
+    server.put(`/users/${username}`, userData),
+  deleteUser: (username) => server.delete(`/users/${username}`),
   nextRound: (playersRound, gameId) =>
-    server.patch("/games/next", { playersRound: playersRound, gameId: gameId }),
-  prevRound: (gameId) => server.patch("/games/prev", { gameId: gameId }),
+    server.put("/v1/games/next", { playersRound: playersRound, gameId: gameId }),
+  prevRound: (gameId) => server.put("/v1/games/prev", { id: gameId }),
   finishGame: (players, gameId, user, winner) =>
-    server.patch("/games/finish", {
+    server.put("/v1/games/finish", {
       players: players,
       gameId: gameId,
       user: user,
