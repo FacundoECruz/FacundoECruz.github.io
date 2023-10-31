9dbea3065d5b64fa29aace1ec8ef2f217f68fc71
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-10-31 13:02:02 -0300

    using popover instead of tooltip

--- src/components/achievements/components/Sticker.jsx
+++ src/components/achievements/components/Sticker.jsx
@@ -1,7 +1,21 @@
-import { Tooltip } from "@mui/material";
+import { Box, Popover, Typography } from "@mui/material";
+import { useState } from "react";
 
 /* eslint-disable react/prop-types */
 function Sticker({ img, msg, wasTopImage = null, guinessStyle = null }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
   const imageStyle = {
     width: "25px",
     height: "auto",
@@ -10,14 +24,28 @@ function Sticker({ img, msg, wasTopImage = null, guinessStyle = null }) {
   };
 
   return (
-    <Tooltip title={msg}>
+    <Box sx={{display: "flex"}}>
       <img
         src={img}
+        aria-describedby={id}
+        onClick={handleClick}
         style={
           wasTopImage ? wasTopImage : guinessStyle ? guinessStyle : imageStyle
         }
       />
-    </Tooltip>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+      >
+        <Typography sx={{ p: 2 }}>{msg}</Typography>
+      </Popover>
+    </Box>
   );
 }
 
