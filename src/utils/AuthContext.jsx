9dab88acd265c101cc6b70dc58546eca7f8424cc
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-10-31 12:58:51 -0300

    added data validation

--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -16,42 +16,87 @@ export function AuthProvider({ children, localStorage = window.localStorage }) {
 
   const login = (data) => {
     setLoginError(null);
-    api
-      .login(data)
-      .then((res) => {
-        const user = res.data.username;
-        window.localStorage.setItem("user", user);
-        setUser(user);
-      })
-      .catch((err) => setLoginError(err.response.data));
+    const validateError = validateLoginData(data);
+    if (validateError === "") {
+      api
+        .login(data)
+        .then((res) => {
+          const user = res.data.username;
+          window.localStorage.setItem("user", user);
+          setUser(user);
+        })
+        .catch((err) => {
+          setLoginError(err.response.data);
+        });
+    } else {
+      setLoginError(validateError);
+    }
   };
 
   const register = (formData) => {
     setRegisterError(null);
-    api
-      .createUser(formData)
-      .then((res) => {
-        const user = res.data.username;
-        window.localStorage.setItem("user", user);
-        setUser(user);
-      })
-      .catch((err) => {
-        setRegisterError(err.response.data);
-      });
+    const validateError = validateRegisterData(formData);
+    if (validateError === "") {
+      api
+        .createUser(formData)
+        .then((res) => {
+          const user = res.data.username;
+          window.localStorage.setItem("user", user);
+          setUser(user);
+        })
+        .catch((err) => {
+          setRegisterError(err.response.data);
+        });
+    } else {
+      setRegisterError(validateError);
+    }
   };
 
   const associate = (formData) => {
-    setRegisterError(null)
-    api
-      .associateUser(formData)
-      .then((res) => {
-        const user = res.data.username;
-        window.localStorage.setItem("user", user);
-        setUser(user);
-      })
-      .catch((err) => {
-        setRegisterError(err.response.data);
-      });
+    setRegisterError(null);
+    const validateError = validateRegisterData(formData);
+    if (validateError === "") {
+      api
+        .associateUser(formData)
+        .then((res) => {
+          const user = res.data.username;
+          window.localStorage.setItem("user", user);
+          setUser(user);
+        })
+        .catch((err) => {
+          setRegisterError(err.response.data);
+        });
+    } else {
+      setRegisterError(validateError);
+    }
+  };
+
+  function validateRegisterData({
+    username,
+    password,
+    email = "",
+    image = "",
+  }) {
+    if (username === "" || password === "" || email === "" || image === "")
+      return "Completar todos los campos";
+
+    const regex = /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)*(\.[a-z]{2,4})$/;
+    if (!regex.test(email)) return "Email invalido";
+
+    const minLength = 4;
+    const maxLength = 12;
+    if (password.length <= minLength) return "La contraseña es muy corta";
+    else if (password.length > maxLength) {
+      return "La contraseña es muy larga";
+    }
+    return "";
+  }
+
+  function validateLoginData({ username, password }) {
+    if (username === "" || password === "") 
+      return "Completar todos los campos"
+    else
+      return ""
   }
 
   const logout = () => {
@@ -61,7 +106,15 @@ export function AuthProvider({ children, localStorage = window.localStorage }) {
 
   return (
     <AuthContext.Provider
-      value={{ user, login, logout, loginError, registerError, register, associate }}
+      value={{
+        user,
+        login,
+        logout,
+        loginError,
+        registerError,
+        register,
+        associate,
+      }}
     >
       {children}
     </AuthContext.Provider>
