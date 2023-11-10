e83d8987421c7306a9e8ef9e714ac2636580b2ba
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-11-10 13:56:31 -0300

    highlighting +10 rounds in games section

--- src/pages/games/components/PlayerHistory.jsx
+++ src/pages/games/components/PlayerHistory.jsx
@@ -2,15 +2,30 @@ import { Typography } from "@mui/material";
 
 export function PlayerHistory({ player }) {
   return player.history.map((roundResult, index) => {
-    if (roundResult > 0) {
+    if (roundResult > 0 && roundResult < 10) {
       return (
-        <Typography key={index} sx={{ color: "green", fontSize: "small", mx: "5px" }}>
+        <Typography
+          key={index}
+          sx={{ color: "green", fontSize: "small", mx: "5px" }}
+        >
+          {roundResult}
+        </Typography>
+      );
+    } else if (roundResult > 9) {
+      return (
+        <Typography
+          key={index}
+          sx={{ color: "lightblue", fontSize: "small", mx: "5px" }}
+        >
           {roundResult}
         </Typography>
       );
     } else {
       return (
-        <Typography key={index} sx={{ color: "red", fontSize: "small", mx: "5px" }}>
+        <Typography
+          key={index}
+          sx={{ color: "red", fontSize: "small", mx: "5px" }}
+        >
           {roundResult}
         </Typography>
       );
