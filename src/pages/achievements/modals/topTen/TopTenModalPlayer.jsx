9575395e3fef7a3314fc2649b7c645fa2f4bfcdb
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-11-10 14:02:36 -0300

    extracted styles variable

--- src/pages/achievements/modals/topTen/TopTenModalPlayer.jsx
+++ src/pages/achievements/modals/topTen/TopTenModalPlayer.jsx
@@ -6,19 +6,22 @@ function TopTenPlayer({ data }) {
   const rowStyle = {
     display: "flex",
     flexDirection: "row",
+    marginBottom: "3px",
   };
+
+  const profileImageStyle = {
+    width: "45px",
+    height: "45px",
+    margin: "3px",
+    borderRadius: "50%",
+  }
 
   return (
     <Box sx={rowStyle}>
       <Box sx={{ display: "flex", flexDirection: "row", width: "150px" }}>
         <img
           src={data.image}
-          style={{
-            width: "45px",
-            height: "45px",
-            margin: "3px",
-            borderRadius: "50%",
-          }}
+          style={profileImageStyle}
         />
         <Typography variant="h6" color={"white"}>
           {data.username}
