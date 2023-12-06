0b2513afbf9ea74444600e9b47155afd9b5f4e6d
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-12-06 15:42:01 -0300

    changed finish game wired

--- src/pages/newGame/scores/useScores.jsx
+++ src/pages/newGame/scores/useScores.jsx
@@ -4,7 +4,7 @@ import { types } from "../../../utils/reducerTypes";
 import api from "../../../utils/api-client";
 import Swal from "sweetalert2";
 
-export function useScores() {
+export function useScores(backToForm) {
   const [cardsPerRound, setCardsPerRound] = useState([]);
   const [round, setRound] = useState(null);
   const [status, setStatus] = useState(null);
@@ -23,15 +23,12 @@ export function useScores() {
     setStatus(JSON.parse(window.localStorage.getItem("status")));
   }, [playersRound]);
 
+  // NEXT ROUND
+
   function nextRound() {
     setVarCheck(false);
-
     if (invalidRoundData()) {
-      Swal.fire({
-        icon: "error",
-        title: "Error",
-        text: "Al menos uno tiene que perder",
-      });
+      invalidRoundAlert();
     } else {
       const gameId = window.localStorage.getItem("gameId");
       setRoundStatus("loading");
@@ -43,6 +40,9 @@ export function useScores() {
             newState: res.data.newRoundState,
           });
           updateLocalStorageAfterRound(res.data);
+          if (res.data.round > 9) {
+            finishGame();
+          }
         })
         .catch((err) => {
           console.log(err);
@@ -57,13 +57,17 @@ export function useScores() {
     return playersLost.every((p) => p === 0);
   }
 
+  function invalidRoundAlert() {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Al menos uno tiene que perder",
+    });
+  }
+
   function updateLocalStorageAfterRound(roundData) {
     window.localStorage.setItem("round", roundData.round);
-    if (roundData.round < 10) {
-      window.localStorage.setItem("status", JSON.stringify(roundData.status));
-    } else {
-      window.localStorage.setItem("status", JSON.stringify("finished"));
-    }
+    window.localStorage.setItem("status", JSON.stringify(roundData.status));
     window.localStorage.setItem(
       "players",
       JSON.stringify(roundData.newRoundState)
@@ -71,7 +75,7 @@ export function useScores() {
     updateTable(roundData.newRoundState);
   }
 
-  function updateTable(table){
+  function updateTable(table) {
     const uiTable = table.map((p) => {
       const { username, score, image, history } = p;
       return {
@@ -86,6 +90,48 @@ export function useScores() {
     setTable(uiTable);
     setRoundStatus("idle");
   }
+
+  function finishGame() {
+    const finalTable = JSON.parse(window.localStorage.getItem("table"));
+    const winner = finalTable[0];
+    Swal.fire({
+      title: `Ganó ${winner.username} con ${winner.score} puntos`,
+      width: 600,
+      padding: "3em",
+      customClass: {
+        title: "custom-title-color",
+      },
+      color: "#716add",
+      background:
+        "#eee url(https://res.cloudinary.com/dfknsvqer/image/upload/v1699022974/altisima/win_za9q0s.jpg)",
+      confirmButtonText: "Salir",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        exitGame();
+      }
+    });
+  }
+
+  function exitGame() {
+    cleanupLocalStorage()
+    updateAchievements()
+  }
+
+  function cleanupLocalStorage(){
+    window.localStorage.removeItem("table");
+    window.localStorage.removeItem("cardsPerRound");
+    window.localStorage.removeItem("gameId");
+    window.localStorage.removeItem("round");
+    backToForm()
+  }
+
+  function updateAchievements(){
+    api.getAchievements().then((res) => {
+      window.localStorage.setItem("achievements", JSON.stringify(res.data));
+    });
+  }
+
+  // PREV ROUND
 
   function prevRound() {
     setVarCheck(true);
