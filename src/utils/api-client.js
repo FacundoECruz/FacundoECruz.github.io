6abdf61d3bc6d13f072dd12850925da26211afbe
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-12-18 12:38:32 -0300

    fixed add player to db issue

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -18,25 +18,14 @@ const setAuthToken = (token) => {
 
 const api = {
   getGames: () => server.get("/games"),
-  // createGame: (players) => server.post("/v1/games", players),
   getPlayer: (username) => server.get(`/players/${username}`),
-  // createPlayer: (playerData) => server.post("/v1/players", playerData),
-  // getAssociatedPlayers: (user) => server.get(`/v1/players/${user}`),
   getPlayers: () => server.get("/players"),
   getUnregisteredPlayers: () => server.get("/v1/players"),
-  // getUser: (username) => server.get(`/users/${username}`),
   login: (data) => server.post("/users/login", data),
   createUser: (userData) => server.post("/v1/users", userData),
   getUsers: () => server.get("/users"),
   editUser: (username, userData) => server.put(`/users/${username}`, userData),
-  // associateUser: (userData) => server.post("/v1/users/associate", userData),
   deleteUser: (username) => server.delete(`/users/${username}`),
-  // nextRound: (playersRound, gameId) =>
-  //   server.put("/v1/games/next", {
-  //     playersRound: playersRound,
-  //     gameId: gameId,
-  //   }),
-  // prevRound: (gameId) => server.put("/v1/games/prev", { id: gameId }),
   getAchievements: () => server.get("/achievements"),
   authenticatedRequest: async (url, method, data, token) => {
     setAuthToken(token);
@@ -48,20 +37,9 @@ const api = {
       });
       return response.data;
     } catch (e) {
-      console.log(e)
+      return e;
     }
   },
 };
-
-// {
-//   createGame: (players) => server.post("/v1/games", players),
-//   createPlayer: (playerData) => server.post("/v1/players", playerData),
-//   getAssociatedPlayers: (user) => server.get(`/v1/players/${user}`),
-//   getUser: (username) => server.get(`/users/${username}`),
-//   editUser: (username, userData) => server.put(`/users/${username}`, userData),
-//   associateUser: (userData) => server.post("/v1/users/associate", userData),
-//   nextRound: (playersRound, gameId) =>server.put("/v1/games/next", {playersRound: playersRound,gameId: gameId,}),
-//   prevRound: (gameId) => server.put("/v1/games/prev", { id: gameId }),
-// }
 
 export default api;
